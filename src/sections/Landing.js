726a8c7866c9217667c031f1bf7e69a51146c082
Courtenay <court.eccles@gmail.com>
2018-10-29 23:11:11 -0600

    first commit

---
+++ src/sections/Landing.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Landing() {
+    const Container = styled.section`
+        padding: 4em;
+        background: papayawhip;
+    `;
+    const Title = styled.h1`
+        font-size: 1.5em;
+        text-align: center;
+        color: palevioletred;
+    `;
+    
+    return (
+        <Container>
+            <Title>
+                Woa!
+            </Title>
+        </Container>
+    )
+}
